149c6200f0d816ea9d27d23679ac652fd369e97f
levch <levchenko.d.a1998@gmail.com>
2023-09-04 09:53:34 +0600

    -

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
 export {
   type Class,
   env,
-} from "https://raw.githubusercontent.com/Dmitriy7244/deno-std/main/mod.ts"
+} from "https://raw.githubusercontent.com/Dmitriy7244/deno-std/main/src/mod.ts"
 export { Db, MongoClient, ObjectId } from "npm:mongodb"
